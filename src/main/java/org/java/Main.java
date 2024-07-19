e4e504cf7d04591056352097dd7c562e8140a32a
Basavaraj <basuburli1998@gmail.com>
2024-07-19 15:02:39 +0530

    second time commiting

--- src/main/java/org/java/Main.java
+++ src/main/java/org/java/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.java;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/org/java/sorting/SelectionSort.java
@@ -0,0 +1,4 @@
+package org.java.sorting;
+
+public class SelectionSort {
+}
