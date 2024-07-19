0e19b11e285cafbc22133368fda9d77c776a3ef7
Basavaraj <basuburli1998@gmail.com>
2024-07-19 15:49:49 +0530

    Bubble sort completed but go through logic again

--- src/main/java/org/java/sorting/BubbleSort.java
+++ src/main/java/org/java/sorting/BubbleSort.java
@@ -1,5 +1,22 @@
 package org.java.sorting;
 
 public class BubbleSort {
-    //bubble sort algotithm
+    public static void main(String[] args) {
+        int [] array={23,24,14,9,11,6,2,7,98};
+        for (int i=0;i<array.length-1;i++){
+            int smallest=i;
+            for (int j=i+1;j<array.length;j++){
+                if (array[smallest]>array[j]){
+                    smallest=j;
+                }
+            }
+            int temp=array[smallest];
+            array[smallest]=array[i];
+            array[i]=temp;
+        }
+        for(int a:array){
+            System.out.println(a);
+        }
+
+    }
 }
