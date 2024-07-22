8290cc3194815efcb0877e99ad2062c75be1c0ff
Basavaraj <basuburli1998@gmail.com>
2024-07-22 12:26:57 +0530

    ading sum of cube code

--- src/main/java/org/java/practice/Main.java
+++ src/main/java/org/java/practice/Main.java
@@ -2,30 +2,6 @@ package org.java.practice;
 
 public class Main {
     public static void main(String[] args) {
-        int [] num1={1,2,2,1};
-
-        int[] num2={2,2};
-
-        int[] num3=new int[num2.length];
-        int index=0;
-
-        for (int i=0;i<num1.length-1;i++){
-            for (int j=0;j<num2.length-1;j++){
-                if (num1[i]==num2[j]){
-                    if (num3[index]==num1[i]){
-                        break;
-                    }else {
-                        num3[index] = num1[i];
-                        index++;
-                    }
-
-                }
-            }
-        }
-
-        for (int s:num3){
-            System.out.println(s);
-        }
 
     }
 }
---
+++ src/main/java/org/java/trail_version/DuplicateNumber.java
@@ -0,0 +1,33 @@
+package org.java.trail_version;
+
+public class DuplicateNumber {
+    public static void main(String[] args) {
+        int [] num1={1,2,2,1};
+
+        int[] num2={2,2};
+
+        int[] num3=new int[num2.length];
+        int index=0;
+
+        for (int i=0;i<num1.length-1;i++){
+            for (int j=0;j<num2.length-1;j++){
+                if (num1[i]==num2[j]){
+                    if (num3[index]==num1[i]){
+                        break;
+                    }else {
+                        num3[index] = num1[i];
+                        index++;
+                    }
+
+                }
+            }
+        }
+
+        for (int s:num3){
+            System.out.println(s);
+        }
+
+    }
+}
+
+//out put should be duplicate element from the both the table
